fb9c5db307fab837de3be56d8695538159d0f7d3
cornelia-schulz <cornelia.schulz.nz@gmail.com>
2020-06-08 00:11:27 +1000

    initial commit

---
+++ src/containers/Articles.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="uk-section">
+      <div class="uk-container uk-container-large">
+        <h1>Connie's Photography Blog</h1>
+
+        <ArticlesList :articles="articles"></ArticlesList>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ArticlesList from "../components/ArticlesList.vue";
+import gql from "graphql-tag";
+
+export default {
+  components: {
+    ArticlesList
+  },
+  data() {
+    return {
+      articles: []
+    };
+  },
+  apollo: {
+    articles: gql`
+      query Articles {
+        articles {
+          id
+          title
+          media {
+            url
+          }
+          published_at
+          category {
+            name
+          }
+        }
+      }
+    `
+  }
+};
+</script>
